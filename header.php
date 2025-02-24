b6e94f1ff52bc893ed228592895ed835117b2453
brx.igor2 <brx.igor@gmail.com>
2025-02-24 14:53:46 -0300

    transmformado em wordpress

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movimento Muda Sinte - Oposição ao Sinte/RN</title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/responsivo.css" media="screen" />
+
+    <?php wp_head(); ?>
+</head>
--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movimento Muda Sinte - Oposição ao Sinte/RN</title>
-
-    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="responsivo.css" media="screen" />
-
-</head>
+<?php get_header(); ?>
 
 <body>
 
@@ -23,14 +13,14 @@
             <span>MENU</span>
         </div>
             <ul class="nav-list">
-                <li>Notícias</li>
-                <li>Documentos</li>
-                <li>Multimídia</li>
-                <li>Canais e Redes Sociais</li>
-                <li>Política de Privacidade</li>
+                <li><a href="./">Notícias</a></li>
+                <li><a href="">Documentos</a></li>
+                <li><a href="">Multimídia</a></li>
+                <li><a href="">Canais e Redes Sociais</a></li>
+                <li><a href="">Política de Privacidade</a></li>
             </ul>
-            <a class="logo" href="/"><img src="./img/logo.png" alt=""></a>
-            <img class="pesquisa" src="./img/pesquisar.png" alt="">
+            <a class="logo" href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>./img/logo.png" alt=""></a>
+            <img class="pesquisa" src="<?php echo get_stylesheet_directory_uri(); ?>./img/pesquisar.png" alt="">
         </nav>
     </header>
 
@@ -46,60 +36,74 @@
             <div class="colunas">
             <div class="colunaNoticias">
                 <div class="noticiaPrincipal">
-                    <img src="./img/professorabebida.jpg" alt="">
+                    <a href="">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/professorabebida.jpg" alt="">
                     <span class="cidadeNoticia">São Gonçalo</span>
                     <h2>Professora só dorme depois de tomar uma dose de cachaça</h2>
                     <span class="autorNoticia">Por J.Linfonodo</span>
                     <p>A narrativa ainda traz dilemas existenciais: será que é a cachaça que ajuda a dormir, ou é o sono que ajuda a preciar melhor a cachaça? É uma mistura de filosofia de boteco com determinação de maratonista!</p>
+                    </a>
                 </div>
                 <ul class="outrasNoticias">
                     <li>
-                        <img src="./img/telacinza.jpeg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/telacinza.jpeg" alt="">
                         <div>
                             <span class="cidadeNoticia">Natal</span>
                             <h3>Sem saúde, energia e água: Brasil vive o caos das privatizações</h3>
                             <span class="autorNoticia">Por Bruxo da Frasqueira</span>
                         </div>
+                        </a>
                     </li>
                     <li>
-                        <img src="./img/telacinza.jpeg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/telacinza.jpeg" alt="">
                         <div>
                             <span class="cidadeNoticia">Natal</span>
                             <h3>Sem saúde, energia e água: Brasil vive o caos das privatizações</h3>
                             <span class="autorNoticia">Por Bruxo da Frasqueira</span>
                         </div>
+                        </a>
                     </li>
                     <li>
-                        <img src="./img/telacinza.jpeg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/telacinza.jpeg" alt="">
                         <div>
                             <span class="cidadeNoticia">Natal</span>
                             <h3>Sem saúde, energia e água: Brasil vive o caos das privatizações</h3>
                             <span class="autorNoticia">Por Bruxo da Frasqueira</span>
                         </div>
+                        </a>
                     </li>
                     <li>
-                        <img src="./img/telacinza.jpeg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/telacinza.jpeg" alt="">
                         <div>
                             <span class="cidadeNoticia">Natal</span>
                             <h3>Sem saúde, energia e água: Brasil vive o caos das privatizações</h3>
                             <span class="autorNoticia">Por Bruxo da Frasqueira</span>
                         </div>
+                        </a>
                     </li>
                     <li>
-                        <img src="./img/telacinza.jpeg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/telacinza.jpeg" alt="">
                         <div>
                             <span class="cidadeNoticia">Natal</span>
                             <h3>Sem saúde, energia e água: Brasil vive o caos das privatizações</h3>
                             <span class="autorNoticia">Por Bruxo da Frasqueira</span>
                         </div>
+                        </a>
                     </li>
                     <li>
-                        <img src="./img/telacinza.jpeg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/telacinza.jpeg" alt="">
                         <div>
                             <span class="cidadeNoticia">Natal</span>
                             <h3>Sem saúde, energia e água: Brasil vive o caos das privatizações</h3>
                             <span class="autorNoticia">Por Bruxo da Frasqueira</span>
                         </div>
+                        </a>
                     </li>
                 
                 </ul>
@@ -109,31 +113,39 @@
                 <ul>
 
                     <li>
-                        <img src="./img/etvarginha.jpg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/etvarginha.jpg" alt="">
                         <span class="categoria">Artigo</span>
                         <h3>ET de Varginha foi realmente visto no bairro das Rocas?</h3>
                         <span class="autorNoticia">Por Paulinho PSTU</span>
+                        </a>
                     </li>
 
                     <li>
-                        <img src="./img/fotosmonalisa.jpg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/fotosmonalisa.jpg" alt="">
                         <span class="categoria">Artigo</span>
                         <h3>Das massas às bolhas: problemas conceituais em uma noção difusa</h3>
                         <span class="autorNoticia">Por Elba Tiúma</span>
+                        </a>
                     </li>
 
                     <li>
-                        <img src="./img/macacodedo.jpg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/macacodedo.jpg" alt="">
                         <span class="categoria">Artigo</span>
                         <h3>O homem ainda faz o que o macaco fazia</h3>
                         <span class="autorNoticia">Por Kuka Beludo</span>
+                        </a>
                     </li>
 
                     <li>
-                        <img src="./img/bruno.jpg" alt="">
+                        <a href="">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/bruno.jpg" alt="">
                         <span class="categoria">Artigo</span>
                         <h3>O time até que é bom, o que mata é o goleiro</h3>
                         <span class="autorNoticia">Por Balan Sah Hola</span>
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -142,12 +154,12 @@
                 <div class="container" id="agenda">
                     <div class="calendar">
                       <div class="month">
-                        <i class="prev"><img src="./img/setaEsquerda.png" alt=""></i>
+                        <i class="prev"><img src="<?php echo get_stylesheet_directory_uri(); ?>./img/setaEsquerda.png" alt=""></i>
                         <div class="date">
                           <h1></h1>
                           <p class="montserrat-font"></p>
                         </div>
-                        <i class="next"><img src="./img/setaDireita.png" alt=""></i>
+                        <i class="next"><img src="<?php echo get_stylesheet_directory_uri(); ?>./img/setaDireita.png" alt=""></i>
                       </div>
                       <div class="weekdays" style="font-size: .5rem; font-weight: 900; color: rgb(43, 43, 43);">
                         <div style="color: rgb(253, 39, 39)">D</div>
@@ -173,8 +185,9 @@
         <section></section>
 
     </main>
-    
+
+    <?php wp_footer(); ?>
 </body>
-<script src="script.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/script.js"></script>
 
 </html>
