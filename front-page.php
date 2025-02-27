a4dfc8c8cbebc8402d0d80779b928fc3be7b9a22
igorbrx2 <igorgomes.rn@gmail.com>
2025-02-27 10:34:07 -0300

    criada pagina de posts >criar multimidia e ajustar header

--- front-page.php
+++ front-page.php
@@ -4,30 +4,6 @@
 
 <?php get_header(); ?>
 
-<body>
-
-    <header>
-        <nav>
-            <div class="menuBtn">
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line1"></div>
-                <div class="line1"></div>
-            </div>
-            <span>MENU</span>
-        </div>
-            <ul class="nav-list">
-                <li><a href="./">Notícias</a></li>
-                <li><a href="">Documentos</a></li>
-                <li><a href="">Multimídia</a></li>
-                <li><a href="">Canais e Redes Sociais</a></li>
-                <li><a href="">Política de Privacidade</a></li>
-            </ul>
-            <a class="logo" href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>./img/logo.png" alt=""></a>
-            <img class="pesquisa" src="<?php echo get_stylesheet_directory_uri(); ?>./img/pesquisar.png" alt="">
-        </nav>
-    </header>
-
     <main>
 
         <div class="paginaAtiva">
