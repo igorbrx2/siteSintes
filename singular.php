98cd6843771be862c938833e6395cd7614db4081
igorbrx2 <igorgomes.rn@gmail.com>
2025-03-21 09:08:29 -0300

    ajustada responsividade e noticias

--- singular.php
+++ singular.php
@@ -4,10 +4,37 @@
 
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<ul class="linksPosts">
+    <li><a href="/home">HOME</a></li>
+    <li><span>|</span></li>
+    <li><a href="/noticias">Notícias</a></li>
+</ul>
 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	
+	<article id="corpoPosts">
+
+	<div class="tituloPost">
 	<?php the_title(); ?>
+	</div>
+
+	<div class="divisorTitulo"></div>
+    <span class="cidadeNoticia">
+	<?php 
+        $categories = get_the_category();
+        if ( ! empty( $categories ) ) {
+            echo esc_html( $categories[0]->name ); 
+        }
+        ?>
+	</span>
+    <span class="autorNoticia"><?php the_author(); ?></span>
+
+	<div class="corpoTexto">
 	<?php the_content(); ?>
+	</div>
+
+	</article>
+
 
 <?php endwhile; else: ?>
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
