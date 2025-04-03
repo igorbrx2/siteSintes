261ec38f7c1d2d4cba38f39bde5b000d207cbca8
igorbrx2 <igorgomes.rn@gmail.com>
2025-04-03 10:48:31 -0300

    criado mídias sociais. >criar páginas de cada categoria

--- page-midiassociais.php
+++ page-midiassociais.php
@@ -4,6 +4,14 @@
 
 <?php get_header(); ?>
 
+<h2 class="midiash2">MÍDIAS SOCIAIS</h2>
+
+<ul class="botoesMidias">
+	<li><a href="#midiasMMM">MMS</a></li>
+	<li><a href="#midiasSGA">São Gonçalo do Amarante</a></li>
+	<li><a href="#midiasCM">Ceará-Mirim</a></li>
+	<li><a href="#midiasUZL">Umarizal</a></li>
+</ul>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	
