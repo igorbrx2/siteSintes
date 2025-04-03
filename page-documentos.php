b30fa8b1f8c00a17daa849cacf7f33e859983a6f
igorbrx2 <igorgomes.rn@gmail.com>
2025-04-03 16:50:12 -0300

    feito documentos. >ajustar margens e espaçamentos

--- page-documentos.php
+++ page-documentos.php
@@ -32,39 +32,43 @@ if ($query->have_posts()) :
 endif; ?>
 
 <?php
-$query = new WP_Query([
-    'category_name'  => 'documentos',
-    'posts_per_page' => 10
-]);
+$args = array(
+    'category_name' => 'documentos',
+    'posts_per_page' => -1,
+    'post_status' => 'publish'
+);
 
-if ($query->have_posts()) :
-    while ($query->have_posts()) : $query->the_post(); ?>
+$documentos_query = new WP_Query($args);
 
-<main class="todosDoc">
-    <div class="cardDoc">
-
-    <a href="<?php the_permalink(); ?>">
-        <?php if (has_post_thumbnail()) : ?>
-            <img src="<?php the_post_thumbnail_url(); ?> "alt="<?php the_title(); ?>">
-            <?php endif; ?>
-        </a>
-
-    <h2><?php the_title(); ?></h2>
-    <p><?php echo wp_trim_words(get_the_content(), 20, '...'); ?></p>
-    <a href="<?php the_permalink(); ?>">Acessar o doc...</a>
-    </div>
-</main>
-
-<?php endwhile;
-wp_reset_postdata();
-endif; ?>
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
-<?php the_content(); ?>
-
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+if ($documentos_query->have_posts()) : ?>
+    <main class="todosDoc">
+        <?php while ($documentos_query->have_posts()) : $documentos_query->the_post(); ?>
+            <div class="cardDoc">
+                <a href="<?php the_permalink(); ?>">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <?php the_post_thumbnail('medium', array('alt' => get_the_title())); ?>
+                    <?php else : ?>
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/placeholder.jpg" alt="<?php the_title(); ?>">
+                    <?php endif; ?>
+                </a>
+                
+                <h2><?php the_title(); ?></h2>
+                
+                <p>
+                    <?php 
+                    echo wp_trim_words(get_the_content(), 20, '...'); 
+                    ?>
+                </p>
+                
+                <a href="<?php the_permalink(); ?>" class="btn-doc">Acessar o doc...</a>
+            </div>
+        <?php endwhile; ?>
+    </main>
+    
+    <?php wp_reset_postdata(); ?>
+<?php else : ?>
+    <p>Nenhum documento encontrado.</p>
 <?php endif; ?>
+
 
 <?php get_footer(); ?>
